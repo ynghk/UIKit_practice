47b9244ca3f8171e5951be10a910d959d7c0efe5
YNGHK <celebi3@naver.com>
2025-03-13 20:16:51 +0900

    UIViewDemo 연습

---
+++ UIViewDemoPractice/UIViewDemoPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIViewDemoPractice
+//
+//  Created by Yung Hak Lee on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
